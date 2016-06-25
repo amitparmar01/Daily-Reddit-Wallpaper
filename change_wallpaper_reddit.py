ecb805ec3dbbd5c572fb3b988ead40b282056c31
amitparmar01 <parmar.amit@gmail.com>
2016-06-25 15:55:15 +0530

    Update change_wallpaper_reddit.py

--- change_wallpaper_reddit.py
+++ change_wallpaper_reddit.py
@@ -7,20 +7,43 @@ import argparse
 import ctypes
 import platform
 import random
+from PIL import Image
 
-# Argument parser
-parser = argparse.ArgumentParser()
-subs = r"wallpapers+wallpaper+WQHD_Wallpaper+topwalls+multiwall+gmbwallpapers+gmbwallpapers"
-parser.add_argument("-s", "--subreddit", type=str, default=subs)
-parser.add_argument("-t", "--time", type=str, default="day")
-args = parser.parse_args()
+
+def main():
+    # Argument parser
+    parser = argparse.ArgumentParser()
+    subs = r"wallpapers+wallpaper+WQHD_Wallpaper+topwalls+multiwall+gmbwallpapers+gmbwallpapers"
+    parser.add_argument("-s", "--subreddit", type=str, default=subs)
+    parser.add_argument("-t", "--time", type=str, default="day")
+    args = parser.parse_args()
+
+    # Python Reddit Api Wrapper
+    print('Get top wallpaper - by /u/ssimunic & /u/admin-mod')
+    print('Sub(s) - {0}'.format(args.subreddit))
+
+    imageLocation = get_image_from_reddit(args.subreddit, args.time)
+
+    if imageLocation:
+        # Check if OS is Linux or Windows, then execute command to change wallpaper
+        platformName = platform.system()
+        if platformName.startswith("Lin"):
+            os.system("gsettings set org.gnome.desktop.background picture-uri file://" + imageLocation)
+            print('Wallpaper changed...')
+        if platformName.startswith("Win"):
+            SPI_SETDESKWALLPAPER = 20
+            ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, imageLocation, 3)
+            print('Wallpaper changed...')
+    else:
+        print('Wallpaper not changed!')
+
 
 # Get image link of most upvoted wallpaper of the day
-def get_top_image(subreddit):
+def get_top_image(subreddit, time):
     urls = []
-    for submission in subreddit.get_top(params={'t': args.time}, limit=20):
+    for submission in subreddit.get_top(params={'t': time}, limit=20):
         url = submission.url
-        if url.endswith(".jpg"):
+        if url.endswith(tuple([".jpg", ".png", ".jpeg"])):
             urls.append(url)
             continue
         # Imgur support
@@ -30,40 +53,65 @@ def get_top_image(subreddit):
             id = url.rsplit("/", 1)[1].rsplit(".", 1)[0]
             urls.append("http://imgur.com/" + id + ".jpg")
             continue
-
     return urls
 
-# Python Reddit Api Wrapper
-r = praw.Reddit(user_agent="Get top wallpaper from /r/" + args.subreddit + " by /u/ssimunic")
 
-# Get top image path
-imageUrls = get_top_image(r.get_subreddit(args.subreddit))
-imageUrl = imageUrls[random.randint(0, len(imageUrls)-1)]
+def get_image_from_reddit(subreddit, time):
 
-# Request image
-response = requests.get(imageUrl)
+    r = praw.Reddit(user_agent="Get top wallpaper - by /u/ssimunic & /u/admin-mod")
+    # Get top image paths in a list
+    imageUrls = get_top_image(r.get_subreddit(subreddit), time)
 
-# If image is available, proceed to save
-if response.status_code == 200:
-    # Get home directory and location where image will be saved (default location for Ubuntu is used)
-    homedir = os.path.expanduser('~')
-    id = imageUrl.rsplit("/", 1)[1].rsplit(".", 1)[0]
-    saveLocation = homedir + "/Pictures/Wallpapers/" + id + ".jpg"
+    for i in range(0, len(imageUrls)):
+        # Pick a random URL from list
+        random.seed()
+        imageUrl = imageUrls[random.randint(0, len(imageUrls) - 1)]
 
-    # Create folders if they don't exist
-    dir = os.path.dirname(saveLocation)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+        # Request image
+        print('Requesting image - ', imageUrl)
+        response = requests.get(imageUrl)
 
-    # Write to disk
-    with open(saveLocation, 'wb') as fo:
-        for chunk in response.iter_content(4096):
-            fo.write(chunk)
+        # If image is available, proceed to save
+        if response.status_code == 200:
+            print('Image found!')
+            # Get home directory and location where image will be saved (default location for Ubuntu is used)
+            homedir = os.path.expanduser('~')
+            id = imageUrl.rsplit("/", 1)[1].rsplit(".", 1)[0]
+            saveLocation = homedir + "/Pictures/Wallpapers/" + id + ".jpg"
 
-    # Check if OS is Linux or Windows, then execute command to change wallpaper
-    platformName = platform.system()
-    if platformName.startswith("Lin"):
-        os.system("gsettings set org.gnome.desktop.background picture-uri file://" + saveLocation)
-    if platformName.startswith("Win"):
-        SPI_SETDESKWALLPAPER = 20
-        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, saveLocation, 3)
+            # Create folders if they don't exist
+            dir = os.path.dirname(saveLocation)
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+
+            # Write to disk
+            with open(saveLocation, 'wb') as fo:
+                for chunk in response.iter_content(4096):
+                    fo.write(chunk)
+
+            # Check if HD width>=1900
+            hd = False
+            with Image.open(saveLocation, 'r') as im:
+                width, height = im.size
+                if width < 1920:
+                    print('Image ({0}x{1}) is not HD(width>=1920). Skipping...'.format(width, height))
+                    hd = False
+                else:
+                    print('Image size: {0}x{1}'.format(width, height))
+                    hd = True
+            # If HD Image then all good, return with the location
+            if hd:
+                return saveLocation
+            # If not, then remove the image and try another one
+            else:
+                os.remove(saveLocation)
+                continue
+        else:
+            print('Image not found!')
+
+    # If here, then no good image found.
+    return ''
+
+
+if __name__ == '__main__':
+    main()
